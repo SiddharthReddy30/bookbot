a176eb14368e26bb76cafbd4c26501761527f41e
Siddharth Reddy Thudi <siddharthreddythudi@icloud.com>
2024-01-23 02:59:17 -0500

    Add report generator

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     print(text)
     print(f'No of words = {get_book_word_count(text)}')
     print(f'Alphabet count = {str(get_alphabets_count(text))}')
-
+    generate_report(book_path, text)
 def get_book_text(bookpath: Path):
     with open(bookpath) as f:
         return f.read()
@@ -23,5 +23,14 @@ def get_alphabets_count(text: str):
         else:
             alphabet_count[lower_letter]=1
     return alphabet_count
+
+def generate_report(book_path: str, booktext: str):
+    print(f'--- Begin report of {book_path} ---')
+    print(f'{get_book_word_count(booktext)} words found in the document\n')
+    sorted_dict = dict(sorted(get_alphabets_count(booktext).items(),key= lambda x:x[1],reverse=True))
+    for letter,count in sorted_dict.items():
+        if letter.isalpha():
+            print(f'The \'{letter}\' character was found {count} times')
+    print('-- End report --')
 
 main()
