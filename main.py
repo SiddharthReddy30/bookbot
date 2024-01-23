935827cc2df68b92d8fa695b8b9f0985b198e357
Siddharth Reddy Thudi <siddharthreddythudi@icloud.com>
2024-01-23 00:51:06 -0500

    Add each character count funtionality

--- main.py
+++ main.py
@@ -4,11 +4,23 @@ def main():
     text = get_book_text(filePath)
     print(text)
     print(f'No of words = {get_book_word_count(text)}')
-    
+    print(f'Alphabet count = {str(get_alphabets_count(text))}')
+
 def get_book_text(bookpath):
     with open(bookpath) as f:
         return f.read()
 
 def get_book_word_count(text):
     return len(text.split())
+
+def get_alphabets_count(text: str):
+    alphabet_count = {}
+    for letter in text:
+        lower_letter = letter.lower()
+        if lower_letter in alphabet_count:
+            alphabet_count[lower_letter] += 1
+        else:
+            alphabet_count[lower_letter]=1
+    return alphabet_count
+
 main()
